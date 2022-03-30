ab8a084d56e1e9f9c31acfb3337386df0c87c5cf
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2022-03-30 17:22:13 +0545

    Fix the git changes errors

---
+++ intergrate-wpforms-and-mailercloud.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Integrate WPForms And Mailercloud
+ * Description: Mailercloud integration with WPForms.
+ * Version: 1.0.0
+ * Author: Sanjeev Aryal
+ * Author URI: https://www.sanjeebaryal.com.np
+ * Text Domain: integrate-wpforms-and-mailercloud
+ */
+
+defined( 'ABSPATH' ) || die();
+
+define( 'INTEGRATE_WPFORMS_AND_MAILERCLOUD_PLUGIN_FILE', __FILE__ );
+define( 'INTEGRATE_WPFORMS_AND_MAILERCLOUD_PLUGIN_PATH', __DIR__ );
+
+/**
+ * Plugin version.
+ */
+const INTEGRATE_WPFORMS_AND_MAILERCLOUD_VERSION = '1.0.0';
+
+add_action(
+	'wpforms_loaded',
+	function() {
+
+	require_once INTEGRATE_WPFORMS_AND_MAILERCLOUD_PLUGIN_PATH . '/src/class-mailercloud.php';
+    
+    // Load translated strings.
+    load_plugin_textdomain( 'integrate-wpforms-and-mailercloud', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+	}
+);
